dece27d318d10c737c51ead726bb617675626e60
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-03-29 23:02:20 -0300

    acessando atributos e composicao objetos

--- 03.Orientacao-a-Objetos-parte-1/Carro.java
+++ 03.Orientacao-a-Objetos-parte-1/Carro.java
@@ -1,23 +1,11 @@
 
-public class Principal2 {
+public class Carro {
 	
-	public static void main(String[] args) {
-		
-		Proprietario dono1 = new Proprietario();
-		dono1.nome = "João";
-		dono1.cpf = "000.000.000-00";
-		dono1.idade = 26;
-		dono1.logradouro = "Rua João Pinheiro, 10";
-		dono1.bairro = "Centro";
-		dono1.cidade = "Uberlandia";
-		
-		Carro meuCarro = new Carro();
-		meuCarro.fabricante = "Fiat";
-		meuCarro.modelo = "Palio";
-		meuCarro.cor = "Prata";
-		meuCarro.anoDeFabricacao = 2012;
-		meuCarro.dono = dono1;
-		
-	}
+	String fabricante;
+	String modelo;
+	String cor;
+	int anoDeFabricacao;
+	
+	Proprietario dono;
 
 }
--- 03.Orientacao-a-Objetos-parte-1/Principal2.java
+++ 03.Orientacao-a-Objetos-parte-1/Principal2.java
@@ -4,7 +4,7 @@ public class Principal2 {
 	public static void main(String[] args) {
 		
 		Proprietario dono1 = new Proprietario();
-		dono1.nome = "João";
+		dono1.nome = "João Pedro";
 		dono1.cpf = "000.000.000-00";
 		dono1.idade = 26;
 		dono1.logradouro = "Rua João Pinheiro, 10";
@@ -17,6 +17,13 @@ public class Principal2 {
 		meuCarro.cor = "Prata";
 		meuCarro.anoDeFabricacao = 2012;
 		meuCarro.dono = dono1;
+		
+		System.out.println("-----Meu carro-----");
+		System.out.println("Dono: " + meuCarro.dono.nome);
+		System.out.println("Fabricante: " + meuCarro.fabricante);
+		System.out.println("Modelo: " + meuCarro.modelo);
+		System.out.println("Cor: " + meuCarro.cor);
+		System.out.println("Ano: " + meuCarro.anoDeFabricacao);
 		
 	}
 
